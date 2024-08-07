700ff0e6057076e51cace4d924b33c244766ee65
YaguarEgor <Egor072005@yandex.ru>
2024-08-08 00:25:29 +0300

    quick-game button added

---
+++ DurakTG_App/src/app/common_ui/quick-game-large/quick-game-large.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-game-large',
+  standalone: true,
+  imports: [],
+  templateUrl: './quick-game-large.component.html',
+  styleUrl: './quick-game-large.component.scss'
+})
+export class QuickGameLargeComponent {
+
+}
--- DurakTG_App/src/app/pages/main-page/main-page.component.ts
+++ DurakTG_App/src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProfileCardComponent } from '../../common_ui/profile-card/profile-card.component';
 import { AdvertBannerComponent } from '../../common_ui/advert-banner/advert-banner.component';
+import { QuickGameLargeComponent } from '../../common_ui/quick-game-large/quick-game-large.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [ProfileCardComponent, AdvertBannerComponent],
+  imports: [ProfileCardComponent, AdvertBannerComponent, QuickGameLargeComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
