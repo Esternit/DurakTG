cfb2abfb723ccf59515ceddee886e9d20b6294e0
YaguarEgor <Egor072005@yandex.ru>
2024-08-07 22:21:56 +0300

    advert card added

---
+++ DurakTG_App/src/app/common_ui/advert-banner/advert-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advert-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './advert-banner.component.html',
+  styleUrl: './advert-banner.component.scss'
+})
+export class AdvertBannerComponent {
+
+}
--- DurakTG_App/src/app/pages/main-page/main-page.component.ts
+++ DurakTG_App/src/app/pages/main-page/main-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProfileCardComponent } from '../../common_ui/profile-card/profile-card.component';
+import { AdvertBannerComponent } from '../../common_ui/advert-banner/advert-banner.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [ProfileCardComponent, AdvertBannerComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
