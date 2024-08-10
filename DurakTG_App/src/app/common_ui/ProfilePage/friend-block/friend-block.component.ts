836c530a0c35c14b12d67fc45c03bbeb3d882186
YaguarEgor <Egor072005@yandex.ru>
2024-08-10 20:09:18 +0300

    profile_page done

---
+++ DurakTG_App/src/app/common_ui/ProfilePage/friend-block/friend-block.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './friend-block.component.html',
+  styleUrl: './friend-block.component.scss'
+})
+export class FriendBlockComponent {
+
+}
--- DurakTG_App/src/app/pages/profile-page/profile-page.component.ts
+++ DurakTG_App/src/app/pages/profile-page/profile-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProfileCardComponent } from '../../common_ui/profile-card/profile-card.component';
-
+import { FriendBlockComponent } from '../../common_ui/ProfilePage/friend-block/friend-block.component';
+import { AdvertBannerComponent } from '../../common_ui/ProfilePage/advert-banner/advert-banner.component';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [ProfileCardComponent],
+  imports: [ProfileCardComponent, FriendBlockComponent, AdvertBannerComponent],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss'
 })
